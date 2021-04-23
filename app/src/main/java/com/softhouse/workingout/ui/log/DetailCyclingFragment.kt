85240d88555f3b6500cf3eed659fc9e330f9a355
William9923 <williamong1400@gmail.com>
2021-04-24 05:53:11 +0700

    chore : Done with maps view

---
+++ app/src/main/java/com/softhouse/workingout/ui/log/DetailCyclingFragment.kt
@@ -0,0 +1,64 @@
+package com.softhouse.workingout.ui.log
+
+import android.content.pm.ActivityInfo
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.softhouse.workingout.R
+import com.softhouse.workingout.databinding.FragmentDetailCyclingBinding
+import com.softhouse.workingout.databinding.FragmentTrackingBinding
+
+class DetailCyclingFragment : Fragment() {
+
+    private val viewModel: DetailCyclingViewModel by viewModels()
+
+    lateinit var binding: FragmentDetailCyclingBinding
+
+    private var map: GoogleMap? = null
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailCyclingBinding.inflate(inflater, container, false)
+        // Make screen orientation always portrait
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync {
+            callback
+        }
+    }
+}
---
+++ app/src/main/java/com/softhouse/workingout/ui/log/DetailCyclingViewModel.kt
@@ -0,0 +1,7 @@
+package com.softhouse.workingout.ui.log
+
+import androidx.lifecycle.ViewModel
+
+class DetailCyclingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
