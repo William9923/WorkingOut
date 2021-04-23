bfe99d981343de0880fd313281e2f095fdc6c7fd
William9923 <williamong1400@gmail.com>
2021-04-24 05:25:53 +0700

    Hotfix layout for detail running result

--- app/src/main/java/com/softhouse/workingout/ui/MainActivity.kt
+++ app/src/main/java/com/softhouse/workingout/ui/MainActivity.kt
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         // Setup link for appbar configuration
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_news, R.id.navigation_tracking, R.id.navigation_calender, R.id.navigation_notifications
+                R.id.navigation_news, R.id.navigation_tracking, R.id.navigation_calender, R.id.navigation_running_detail
             )
         )
         // setup navigation controller with the appbar
